460a5fec7489be141964ac07618f881273873a46
BlaznikRaul <123101962+BlaznikRaul@users.noreply.github.com>
2024-04-07 21:49:59 +0200

    Update Programska_koda.ino

--- Programska_koda.ino
+++ Programska_koda.ino
@@ -1,1 +1,22 @@
-Koda
+const int ledPin = 8; 
+const int ldrPin = A3;
+
+void setup() 
+{ 
+  Serial.begin(9600);
+  pinMode (ledPin, OUTPUT);
+  pinMode (ldrPin, INPUT);
+}
+
+void loop() 
+{
+  int ldrStatus = analogRead(1drPin); 
+  if (ldrStatus <= 300) 
+  {
+    digitalWrite(ledPin, HIGH);
+  } 
+  else 
+  {
+  	  digitalWrite(ledPin, LOW);
+  }
+}
